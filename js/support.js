df25b6de7187baa807a4c7b3c50db200f4aa1562
TCxDouglas <douglasalexander683@gmail.com>
2021-06-08 03:16:00 -0600

    Envio de email de contacto

---
+++ js/support.js
@@ -0,0 +1,30 @@
+function supportEmail(){
+    console.log("Enviando...")
+    var urlSupport = "http://localhost:3900/supportEmail";
+    var data = {
+        userSupport: 'Douglas Hernandez',
+        emailSupport: 'douglasalexander683@gmail.com',
+        nameContact: getTextItem("txtName"),
+        lastContact: getTextItem("txtLast"),
+        emailContact: getTextItem("txtEmail"),
+        phoneContact: getTextItem("txtPhone"),
+        message: getTextItem("txtMessage")
+    }
+
+    fetch(urlSupport, {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      }).then(res => res.json())
+      .catch(error => console.error('Error:', error))
+      .then(response => console.log('Success:', response));
+}
+
+
+function getTextItem(id){
+    let text = document.getElementById(id).value;
+
+    return text
+}
